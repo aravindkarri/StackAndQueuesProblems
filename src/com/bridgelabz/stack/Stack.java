0ebab4286028d3b98bb2d95ae4ab4bfd8c648d30
aravindkarri <aravindkarri98495@gmail.com>
2021-07-03 00:07:18 +0530

    ADDED: Code to peak and pop from stack till it is empty

--- src/com/bridgelabz/stack/Stack.java
+++ src/com/bridgelabz/stack/Stack.java
@@ -16,7 +16,39 @@ public class Stack<T>
 		newNode.next = head;
 		head = newNode;
 	}
-	
+	/**
+	 * Method to return top element of the stack
+	 * @return
+	 */
+	public T peek()
+	{
+		return head.data;
+	}
+	/**
+	 * Method to delete the top most element of the stack
+	 * @return
+	 */
+	public T pop()
+	{
+		StackNode<T> tempNode = head;
+		head = head.next;
+		return tempNode.data;
+	}
+	/**
+	 * Method to check stack is empty or not
+	 * @return
+	 */
+	public void isEmpty()
+	{
+		if(head == null)
+		{
+			System.out.println("Stack is empty");
+		}
+		else
+		{
+			System.out.println("Stack is not empty");
+		}
+	}
 	/**
 	 * Method to print stack
 	 */
--- src/com/bridgelabz/stack/StackMain.java
+++ src/com/bridgelabz/stack/StackMain.java
@@ -8,9 +8,20 @@ public class StackMain
 		newStack.push(70);
 		newStack.push(30);
 		newStack.push(56);
-		
 		newStack.printStack();
-
+		
+		//Performing pop and peek operation till stack is empty
+		int topElement = newStack.peek();
+		System.out.println("The element at the top of the stack is : "+topElement);
+		int deletedElement = newStack.pop();
+		System.out.println("Deleted top most element is :" +deletedElement);
+		newStack.peek();
+		newStack.pop();
+		newStack.peek();
+		newStack.pop();
+		newStack.printStack();
+		newStack.isEmpty();
+		
 	}
 
 }
