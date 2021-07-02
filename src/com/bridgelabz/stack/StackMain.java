82fafeca2488f7dbf8f675a4e87a8d14aa46f88a
aravindkarri <aravindkarri98495@gmail.com>
2021-07-02 23:18:19 +0530

    ADDED: Code to create a simple stack

---
+++ src/com/bridgelabz/stack/StackMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.stack;
+
+public class StackMain 
+{
+	public static void main(String[] args)
+	{
+		Stack<Integer> newStack = new Stack<>();
+		newStack.push(70);
+		newStack.push(30);
+		newStack.push(56);
+		
+		newStack.printStack();
+
+	}
+
+}
